879e440d3c80085884ec31a378c41b823cd1fb7b
dennetwebdesigner <dennetwebdesigner@gmail.com>
2023-05-30 14:41:47 -0300

    [ui] Address route form change

--- src/pages/AdressesAdd.vue
+++ src/pages/AdressesAdd.vue
@@ -4,37 +4,90 @@
       <Input
         label="Nome da Rota"
         input-size="w-full"
-        v-on:get-value="(e) => (data.name = e)"
+        v-on:get-value="
+          (e) => {
+            data.name = e;
+          }
+        "
         :value="data.name"
       />
-      <button
-        type="button"
-        class="w-5/6 bg-blue-300 text-black py-3 text-xl rounded-lg hover:text-white"
-        @click="() => data.count.push('')"
-      >
-        Clique aqui - Adicionar Novo Endereço
-      </button>
-
-      <div
-        class="w-full flex justify-center items-center"
-        v-for="(_, i) in data.count.length"
-        :key="i"
-      >
+      <div class="w-full flex flex-wrap justify-center items-center">
         <Input
-          v-if="data.count[i] != null"
-          :label="`Endereço ` + (i + 1)"
-          input-size="w-4/6"
-          v-on:get-value="(e) => (data.count[i] = e)"
-          :value="data.count[i]"
+          label="Rua"
+          input-size="w-full"
+          v-on:get-value="
+            (e) => {
+              data.count.street = e;
+            }
+          "
+          :value="data.count.street"
+        />
+
+        <Input
+          label="Bairro"
+          input-size="w-5/12"
+          v-on:get-value="
+            (e) => {
+              data.count.destrict = e;
+            }
+          "
+          :value="data.count.destrict"
+        />
+        <Input
+          label="Número"
+          input-size="w-5/12"
+          v-on:get-value="
+            (e) => {
+              data.count.number = e;
+            }
+          "
+          :value="data.count.number"
+        />
+        <Input
+          label=""
+          input-size="w-5/12 hidden"
+          v-on:get-value="
+            (e) => {
+              data.count.index = e;
+            }
+          "
+          :value="data.count.index"
         />
         <button
-          v-if="data.count[i] != null"
           type="button"
-          class="w-1/12 bg-blue-300 py-2 font-bold text-white self-center mt-6"
-          @click="() => removeInput(i as any as number)"
+          class="w-6/12 bg-blue-300 py-2 font-bold text-white self-center mt-6"
+          @click="moreAddressForm"
         >
-          -
+          {{
+            !data.count.index ? "Adicionar novo endereço" : "Atualizar Endereço"
+          }}
         </button>
+      </div>
+      <div
+        class="w-full mt-3 text-black p-2 bg-white flex justify-between items-center border-b border-slate-300"
+        v-for="(address, i) in data.addresses"
+        :key="i"
+      >
+        <p>
+          {{ address.street }} - {{ address.number }},
+          {{ address.street }}
+        </p>
+        <div>
+          <button
+            type="button"
+            @click="() => edtAddressForm(i)"
+            class="bg-yellow-300 py-1 px-2 text-lg mr-2"
+          >
+            edt
+          </button>
+          <button
+            type="button"
+            @click="() => rmAddressForm(i)"
+            class="bg-red-300 py-1 px-2 text-lg"
+          >
+            rm
+          </button>
+        </div>
       </div>
     </FormContainer>
   </MainContainer>
@@ -44,14 +97,50 @@ import MainContainer from "@components/MainContainer.vue";
 import FormContainer from "@components/Form/FormContainer.vue";
 import Input from "@components/Form/Input.vue";
 import { reactive } from "vue";
+
+type iAddressForm = {
+  street: string;
+  destrict: string;
+  number: string;
+  index: number | null;
+};
+
 const data = reactive({
   name: "",
-  count: [] as any[],
+  count: { street: "", destrict: "", number: "", index: null } as iAddressForm,
+  addresses: [] as Omit<iAddressForm, "index">[],
 });
 
-const removeInput = (i: number) => {
-  console.log(i);
-  data.count[i] = null;
+const rmAddressForm = (i: number) => {
+  data.addresses.splice(i, 1);
 };
+
+const edtAddressForm = (i: number) => {
+  data.count.street = data.addresses[i].street;
+  data.count.destrict = data.addresses[i].destrict;
+  data.count.number = data.addresses[i].number;
+  data.count.index = i;
+};
+
+const moreAddressForm = () => {
+  if (!data.count.street || !data.count.destrict || !data.count.number) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+  const newData = JSON.parse(JSON.stringify(data.count));
+  delete newData.index;
+
+  if (!data.count.index) {
+    data.addresses.push(newData);
+  } else {
+    data.addresses[data.count.index] = newData;
+  }
+
+  data.count.street = "";
+  data.count.number = "";
+  data.count.destrict = "";
+  data.count.index = null;
+};
+
 const handleSubmit = () => {};
 </script>

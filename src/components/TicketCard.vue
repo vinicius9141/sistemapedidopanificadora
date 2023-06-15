635724746415a0d93c5c5a2b9e96f84d86d3905b
dennetwebdesigner <dennetwebdesigner@gmail.com>
2023-06-15 11:10:45 -0300

    Ticket Order Modal edit and remove not working

--- src/components/TicketCard.vue
+++ src/components/TicketCard.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="w-[30%] m-[1%] p-4 bg-[#E9E5E5] rounded-md">
+  <div
+    class="w-[30%] m-[1%] p-4 bg-[#E9E5E5] rounded-md relative"
+    @click="handleHoverOption"
+  >
+    <div class="absolute right-2 top-0 w-2/6">
+      <p class="font-bold text-xl cursor-pointer text-right" data-id="option">
+        ...
+      </p>
+      <div
+        class="absolute right-0 hidden w-full flex-wrap bg-white"
+        ref="optionModalEl"
+      >
+        <router-link
+          class="w-full text-right p-2 hover:text-slate-400"
+          to="#"
+          data-id="option"
+          >editar</router-link
+        >
+        <router-link
+          class="w-full text-right p-2 hover:text-slate-400"
+          to="#"
+          data-id="option"
+          >remover</router-link
+        >
+      </div>
+    </div>
     <p class="text-center my-2">Client: {{ order.client.name }}</p>
     <p class="text-center my-2">Pedidos:</p>
     <p class="text-center my-2" v-for="product in order.products">
@@ -15,7 +40,24 @@
 
 <script lang="ts" setup>
 import { iTocketOrderDTO } from "@/interface/TicketOrderInterface";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+const optionModalEl = ref<HTMLDivElement>();
+
+const handleHoverOption = (e: any) => {
+  if (e.target.dataset["id"] && e.target.dataset["id"] == "option") {
+    e.target.dataset["id"] = "close";
+    optionModalEl.value?.classList.remove("hidden");
+    optionModalEl.value?.classList.add("flex");
+    return;
+  } else {
+    e.target.dataset["id"] = "option";
+    optionModalEl.value?.classList.add("hidden");
+    optionModalEl.value?.classList.remove("flex");
+    return;
+  }
+};
+
 const { order } = defineProps<{ order: iTocketOrderDTO }>();
 onMounted(() => {
   console.log(order);

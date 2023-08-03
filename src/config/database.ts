161073bfc5795682620eafddcb5439d4bbbca882
dennetwebdesigner <dennetwebdesigner@gmail.com>
2023-08-03 12:23:45 -0300

    correction firebase

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
-
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
+import { initializeApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -13,11 +10,9 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.MEASIREMENT_ID
+  measurementId: import.meta.env.MEASIREMENT_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const fireDatabase = getFirestore(app) 
-export const fireAuth = getAuth(app)
+export const fireDatabase = getFirestore(app);
+export const fireAuth = getAuth(app);
